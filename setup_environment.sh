b0d72a87c097761df80118dfd365ce6853534489
dogusyuksel <dogusyuk@gmail.com>
2024-12-02 01:57:49 +0000

    Update setup_environment.sh (#8)
    * Update setup_environment.sh
    
    * Update build-and-tests.yml
    
    * Update build-and-tests.yml

--- setup_environment.sh
+++ setup_environment.sh
@@ -12,55 +12,6 @@ if [ ! -d "thirdparty" ]; then
     mkdir thirdparty
 fi
 
-if [ ! -d "thirdparty/STM32F103X_HAL" ];
-then
-    git clone https://github.com/STMicroelectronics/stm32f1xx_hal_driver.git thirdparty/STM32F103X_HAL
-else
-    update_repo "thirdparty/STM32F103X_HAL"
-fi
-
-if [ ! -d "thirdparty/cmsis-header-stm32" ];
-then
-    git clone https://github.com/modm-io/cmsis-header-stm32.git thirdparty/cmsis-header-stm32
-else
-    update_repo "thirdparty/cmsis-header-stm32"
-fi
-
-if [ ! -d "thirdparty/CMSIS_5" ]; then
-    git clone https://github.com/ARM-software/CMSIS_5.git thirdparty/CMSIS_5
-else
-    update_repo "thirdparty/CMSIS_5"
-fi
-
-if [ ! -d "thirdparty/FreeRTOS-Kernel" ]; then
-    git clone https://github.com/FreeRTOS/FreeRTOS-Kernel.git thirdparty/FreeRTOS-Kernel
-else
-    update_repo "thirdparty/FreeRTOS-Kernel"
-fi
-
-if [ ! -d "thirdparty/linting" ]; then
-    git clone https://github.com/dogusyuksel/embedded_linting.git thirdparty/linting
-else
-    update_repo "thirdparty/linting"
-fi
-
-if [ ! -d "thirdparty/docker" ]; then
-    git clone https://github.com/dogusyuksel/embedded_docker.git thirdparty/docker
-else
-    update_repo "thirdparty/docker"
-fi
-
-if [ ! -d "thirdparty/custom_printf" ]; then
-    git clone https://github.com/mpaland/printf.git thirdparty/custom_printf
-else
-    update_repo "thirdparty/custom_printf"
-fi
-
-if [ ! -d "thirdparty/openocd" ]; then
-    git clone https://github.com/STMicroelectronics/OpenOCD.git thirdparty/openocd
-    cat ./thirdparty/openocd/tcl/board/stm32f103c8_blue_pill.cfg | sed -e "s/set FLASH_SIZE 0x20000/set FLASH_SIZE 0x10000/" > ./thirdparty/openocd/tcl/board/stm32f103c8_custom.cfg
-fi
-
 if [ ! -d "thirdparty/libcanard" ]; then
     git clone https://github.com/OpenCyphal/libcanard.git thirdparty/libcanard
     cd thirdparty/libcanard
