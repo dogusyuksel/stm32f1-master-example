412ba130753b4c79d5501384ba66c17a65933b5f
dogusyuksel <dogusyuk@gmail.com>
2025-08-19 22:32:13 +0100

    env setup bug fix

--- setup_env.sh
+++ setup_env.sh
@@ -2,8 +2,7 @@
 
 cd /workspace && git submodule update --init --recursive
 # cd /workspace/thirdparty/libcanard && git checkout 43de1c4966b8d1e5d57978949d63e697f045b358
-cd /workspacethirdparty/libcanard && git submodule update --init --recursive
-cd /workspacethirdparty && patch -d . -p1 < canard_stm32.patch && patch -d . -p1 < transport.py.patch
-cd /workspace && cat thirdparty/openocd/tcl/board/stm32f103c8_blue_pill.cfg | sed -e "s/set FLASH_SIZE 0x20000/set FLASH_SIZE 0x10000/" > thirdparty/op
-enocd/tcl/board/stm32f103c8_custom.cfg
+cd /workspace/thirdparty/libcanard && git submodule update --init --recursive
+cd /workspace/thirdparty && patch -d . -p1 < canard_stm32.patch && patch -d . -p1 < transport.py.patch
+cd /workspace && cat thirdparty/openocd/tcl/board/stm32f103c8_blue_pill.cfg | sed -e "s/set FLASH_SIZE 0x20000/set FLASH_SIZE 0x10000/" > thirdparty/openocd/tcl/board/stm32f103c8_custom.cfg
 cd /workspace
