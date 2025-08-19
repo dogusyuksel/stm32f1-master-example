511ab44125c047e5caa7053685d6f7eb85d73a72
dogusyuksel <dogusyuk@gmail.com>
2025-08-19 23:20:59 +0100

    relative paths edited

--- application_tool/build.sh
+++ application_tool/build.sh
@@ -10,7 +10,7 @@ if [[ $? = 0 ]]; then
 else
     # then try with my docker
     cd ../..
-    /thirdparty/docker/run_docker.sh golden-sample:latest "cd /workspace/application_tool && rm -rf build && mkdir build && cd build && cmake .. && make"
+    /workspace/thirdparty/docker/run_docker.sh golden-sample:latest "cd /workspace/application_tool && rm -rf build && mkdir build && cd build && cmake .. && make"
     cd -
 fi
 
