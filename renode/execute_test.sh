4a27798acd35fc6247b17a6ba3a993a6928d2cdd
dogusyuksel <dogusyuk@gmail.com>
2025-08-29 16:58:40 +0100

    renode tst bux fixed

--- renode/execute_test.sh
+++ renode/execute_test.sh
@@ -3,9 +3,9 @@
 function renode_test() {
     OUTFILE=$(mktemp)
 
-    renode custom_board.resc > "$OUTFILE" 2>&1
+    renode custom_board_test.resc > "$OUTFILE" 2>&1
 
-    OUTPUT=$(tail -n 10 "$OUTFILE" | tr -d '\r')
+    OUTPUT=$(cat "$OUTFILE" | tr -d '\r')
     echo "$OUTPUT"
 
     if echo "$OUTPUT" | grep -q "Hello, STM32"; then
